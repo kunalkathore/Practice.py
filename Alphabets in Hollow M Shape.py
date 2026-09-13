843be5f41688418e77476cca0c4e7bc41a800ad7
kunal kathore <kunalkathore95@gmail.com>
2026-09-13 20:51:29 +0530

    new file:   Alphabets in Hollow M Shape.py new file:   Diamond - 4.py deleted:    Hollow Diamond - 2.py new file:   Hollow Diamond - 4.py new file:   M Shape with x - 3.py

---
+++ Alphabets in Hollow M Shape.py
@@ -0,0 +1,16 @@
+rows = int(input("Enter the number of rows: ")) #----- Input for the number of rows
+
+#----- Print the hollow M shape pattern of alphabets
+for row in range(1, rows + 1):
+    spaces = " " * (rows - row)
+
+    #----- Print the top half of the hollow M shape pattern
+    if row == 1:
+        each_row = spaces + (chr(row + 64 ) + " ") 
+    else:
+        hollow_spaces = "  " * (row - 2)
+        each_row = spaces + (chr(row + 64) + " ") + hollow_spaces + (chr(row + 64) + " ")
+
+    spaces_between_triangles = " " * (rows - row) #----- Calculate the spaces between the two triangles of the M shape
+    
+    print(each_row + spaces_between_triangles + each_row)
---
+++ Hollow Diamond - 4.py
@@ -0,0 +1,20 @@
+n = int(input("Enter the number of rows: ")) #----- Input for the number of rows
+
+#----- Print the hollow diamond pattern of zeros
+for row in range(1, n + 1):
+    if row == 1:
+        print("* " * (n * 2))
+    else:
+        stars = "* " * ((n - row) + 1)
+        spaces = "  " * ((row * 2) - 2)
+        print(stars + spaces + stars)
+
+#----- Print the bottom half of the hollow diamond pattern of zeros
+for row in range(1, n + 1):
+    if row == 1:
+        space = "  " * ((n - row) * 2)
+        print("* " + space + "* ")
+    else:
+        stars = "* " * row
+        spaces = "  " * ((n - row) * 2)
+        print(stars + spaces + stars)
