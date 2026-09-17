f94f44d5d731e770cd50e00d2a95a5a7589b9d05
kunal kathore <kunalkathore95@gmail.com>
2026-09-17 21:13:39 +0530

    modified:   Comparing Strings & Naming Variables/Roots of a quadratic equation.py

--- Comparing Strings & Naming Variables/Roots of a quadratic equation.py
+++ Comparing Strings & Naming Variables/Roots of a quadratic equation.py
@@ -1,11 +1,9 @@
-import math
-A = int(input("Enter the coefficient of x^2: ")) # Coefficient of x^2
-B = int(input("Enter the coefficient of x: ")) # Coefficient of x
-C = int(input("Enter the constant term: ")) # Constant term
+A = int(input()) # coefficient of x^2
+B = int(input()) # coefficient of x
+C = int(input()) # constant term
 
-# - Calculate the roots of the quadratic equation Ax^2 + Bx + C = 0 using the quadratic formula
-r1 = (-B + math.sqrt(B ** 2 - 4 * A * C)) / (2 * A)
-r2 = (-B - math.sqrt(B ** 2 - 4 * A * C)) / (2 * A)
+r1 = (-B + ((B ** 2 - (4 * A * C)) ** 0.5)) / (2 * A) # First root
+r2 = (-B - ((B ** 2 - (4 * A * C)) ** 0.5)) / (2 * A) # Second root
 
 print(round(r1, 2)) # First root
 print(round(r2, 2)) # Second root
